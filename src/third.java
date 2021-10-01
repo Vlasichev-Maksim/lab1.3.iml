3c7c05f6860573af05e48d1f0e059668f1da69dd
Максим <m.vlasichev@mail.ru>
2021-10-01 10:24:41 +0300

    Initial commit

---
+++ src/third.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class third {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i + " число гарм ряда = " + 1. / i);
+        }
+    }
+}
